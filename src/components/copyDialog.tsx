b0ac40265e6b87f5e859335cb8dd5670fecb39f5
JunHee <jhlee@infracube.co.kr>
2022-11-21 01:48:02 +0900

    copyDialog 수정

---
+++ src/components/copyDialog.tsx
@@ -0,0 +1,74 @@
+import { Dispatch, SetStateAction } from "react";
+import {
+  Link,
+  DialogTitle,
+  Dialog,
+  Container,
+  Typography,
+} from "@mui/material";
+
+type dialogType = {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const CopyDialog = ({ open, setOpen }: dialogType) => {
+  const closeDialog = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Dialog onClose={closeDialog} open={open}>
+        <DialogTitle
+          sx={{ minWidth: "420px", borderBottom: "1px solid #a5a5a5" }}
+        >
+          리소스 출처
+        </DialogTitle>
+        <Container sx={{ padding: "24px" }}>
+          <Typography component="p">
+            시세정보 :{" "}
+            <Link
+              href="https://docs.upbit.com/docs/upbit-quotation-websocket"
+              target="_blank"
+              title="Upbit 공식문서"
+            >
+              업비트 Docs
+            </Link>
+          </Typography>
+          <Typography component="p">
+            애니메이션 :{" "}
+            <Link
+              href="https://lottiefiles.com/"
+              target="_blank"
+              title="LottieFiles"
+            >
+              LottieFiles
+            </Link>
+          </Typography>
+          <Typography component="p">
+            아이콘 :{" "}
+            <Link
+              href="https://react-icons.github.io/react-icons/"
+              target="_blank"
+              title="react-icon"
+            >
+              LottieFiles
+            </Link>
+          </Typography>
+          <Typography component="p">
+            파비콘 :{" "}
+            <Link
+              href="https://www.flaticon.com/free-icons/bitcoin"
+              target="_blank"
+              title="bitcoin icons"
+            >
+              Bitcoin icons created by Freepik - Flaticon
+            </Link>
+          </Typography>
+        </Container>
+      </Dialog>
+    </>
+  );
+};
+
+export default CopyDialog;
--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,38 +1,54 @@
-import { memo } from "react";
-import { Box, Typography, Link } from "@mui/material";
+import { memo, useState } from "react";
+import { Box, Typography, Link, IconButton } from "@mui/material";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import CopyDialog from "../../components/copyDialog";
 
 const Footer = () => {
+  const [open, setOpen] = useState(false);
   const date = new Date();
 
   return (
-    <Box
-      component="footer"
-      sx={{
-        position: "fixed",
-        bottom: "0",
-        left: "0",
-        width: "100%",
-        height: "30px",
-        display: "flex",
-        justifyContent: "flex-end",
-        alignItems: "center",
-      }}
-    >
-      <Typography
-        variant="h3"
-        sx={{ fontSize: "15px", color: "#474E68", textAlign: "right" }}
+    <>
+      <Box
+        component="footer"
+        sx={{
+          position: "fixed",
+          bottom: "0",
+          left: "0",
+          width: "100%",
+          height: "50px",
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
       >
-        <Link
-          href="https://juni-official.tistory.com/"
-          underline="hover"
-          target="_blank"
-          sx={{ color: "#393E46" }}
-          mr={1}
+        <Typography
+          variant="h3"
+          sx={{ fontSize: "14px", color: "#474E68", textAlign: "right" }}
         >
-          {date.getFullYear()}. 꾸생의 DevLog All rights reserved.
-        </Link>
-      </Typography>
-    </Box>
+          <IconButton
+            aria-label="delete"
+            size="small"
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <CopyrightIcon fontSize="inherit" />
+          </IconButton>
+          <Link
+            href="https://juni-official.tistory.com/"
+            underline="hover"
+            target="_blank"
+            sx={{ position: "relative", top: "1px", color: "#393E46" }}
+            ml={0.5}
+            mr={1}
+          >
+            {date.getFullYear()}. 꾸생의 DevLog All rights reserved.
+          </Link>
+        </Typography>
+        <CopyDialog open={open} setOpen={setOpen} />
+      </Box>
+    </>
   );
 };
 
